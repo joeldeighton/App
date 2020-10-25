f71bea8fd084d6959ba62a7096c20c403e52974a
Joel <jcdghtn@gmail.com>
2020-10-25 10:32:24 -0300

    Recently Played - Responsive

--- test-app/src/components/RecentlyPlayed.vue
+++ test-app/src/components/RecentlyPlayed.vue
@@ -3,20 +3,22 @@
     <app-nav></app-nav>
     <h3 class="text-center">Recently Played</h3>
     <hr/>
-    <div class="col-sm-3" v-for="song in recentlyPlayed" v-bind:key="song.name">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title"> {{ song.track.name }} </h3>
-        </div>
-        <div class="container-fluid">
-          <img  v-bind:src="song.track.album.images[1].url" height="300" width="300">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4" v-for="song in recentlyPlayed" v-bind:key="song.name">
+          <div class="panel panel-default">
+            <div class="panel-heaing">
+              <h3 class="panel-title"> {{ song.track.name }} </h3>
+              <span v-for="artist in song.track.album.artists" v-bind:key="artist.id">
+                <a v-bind:href="artist.external_urls.spotify" target="new"> {{ artist.name }} </a>
+              </span>
+            </div>
+            <div>
+              <img class="img-responsive" v-bind:src="song.track.album.images[0].url">
+            </div>
+          </div>
         </div>
       </div>
-      <!-- <span v-for="artist in song.track.album.artists" v-bind:key="artist.name">
-          {{ artist.name }}
-      </span> -->
-    </div>
-    <div class="col-sm-12">
     </div>
   </div>
 </template>
